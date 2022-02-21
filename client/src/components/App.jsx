46bc71284d77cf73604c2484bfeabce98d879dfa
Jean-Luc <jlfelix522@yahoo.com>
2022-02-21 14:03:45 -0800

    Added App.css and PasswordInput.css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PasswordInput from "./PasswordInput.jsx";
-import "../../dist/styles.css";
+import "./App.css";
 
 const App = () => {
 
--- client/src/components/PasswordInput.jsx
+++ client/src/components/PasswordInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../dist/styles.css";
+import "./PasswordInput.css";
 
 const PasswordInput = (props) => {
 
